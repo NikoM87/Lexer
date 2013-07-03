fa540d64ad7bb9233a63af556bdcee5e01855683
Nikolai <nikom87@gmail.com>
2013-07-04 00:48:36 +0600

    Init

---
+++ Lexer/Lexer.cs
@@ -0,0 +1,119 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Lexer
+{
+    public class Lexer
+    {
+        public Stream BaseStream { get; private set; }
+
+        public void LoadTextCode( string code )
+        {
+
+            BinaryWriter writer = new BinaryWriter( BaseStream );
+            BaseStream.Position = 0;
+            writer.Write( Encoding.ASCII.GetBytes( code ) );
+            BaseStream.Position = 0;
+
+            NextChar();
+        }
+
+        private void NextChar()
+        {
+            _nextCh = (char) _reader.Read();
+
+        }
+
+        private readonly StreamReader _reader;
+        private char _nextCh;
+
+        public Lexer( Stream stream )
+        {
+            BaseStream = stream;
+            _reader = new StreamReader( BaseStream );
+            NextChar();
+        }
+
+        public Lexer()
+        {
+            BaseStream = new MemoryStream();
+            _reader = new StreamReader( BaseStream );
+
+        }
+
+        public string Next()
+        {
+            SkipWhitespace();
+
+            if ( IsIntNumber() )
+            {
+                return ReadIntNumber();
+            }
+
+            if ( IsIdentificator() )
+            {
+                return ReadIdentificator();
+            }
+
+            if ( IsOperation() )
+            {
+                return ReadOperation();
+            }
+
+            return "";
+        }
+
+        private void SkipWhitespace()
+        {
+            while ( !_reader.EndOfStream && char.IsWhiteSpace( _nextCh ) )
+            {
+                NextChar();
+            }
+        }
+
+        private string ReadOperation()
+        {
+            var s = "";
+            s += _nextCh;
+            NextChar();
+            return s;
+        }
+
+        private bool IsOperation()
+        {
+            return _nextCh == '+' || _nextCh == '-' || _nextCh == '*' || _nextCh == '/';
+        }
+
+        private string ReadIdentificator()
+        {
+            var s = "";
+            while ( _nextCh == '_' || char.IsLetterOrDigit( _nextCh ) )
+            {
+                s += _nextCh;
+                NextChar();
+            }
+            return s;
+        }
+
+        private bool IsIdentificator()
+        {
+            return _nextCh == '_' || char.IsLetter( _nextCh );
+        }
+
+        private string ReadIntNumber(  )
+        {
+            var s = "";
+            while ( char.IsNumber( _nextCh ) )
+            {
+                s += _nextCh;
+                NextChar();
+            }
+            return s;
+        }
+
+        private bool IsIntNumber()
+        {
+            return char.IsNumber( _nextCh );
+        }
+    }
+}
---
+++ UnitTestProject1/LexerTest.cs
@@ -0,0 +1,94 @@
+﻿using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class LexerTest
+    {
+        private readonly Lexer.Lexer _lex;
+
+        public LexerTest()
+        {
+            _lex = new Lexer.Lexer( );
+        }
+
+        [TestMethod]
+        public void TestStreamLoadTextCode()
+        {
+            Stream stream = new MemoryStream( Encoding.ASCII.GetBytes( "Program" ) );
+            var lex = new Lexer.Lexer( stream );
+          
+            Assert.AreEqual( stream, lex.BaseStream );
+        }
+
+        [TestMethod]
+        public void TestSkipFirstWhitespace()
+        {
+            _lex.LoadTextCode( "\t Program");
+
+            var word = _lex.Next();
+
+            Assert.AreEqual( "Program",word  );
+        }
+
+        [TestMethod]
+        public void TestTwoWord()
+        {
+            _lex.LoadTextCode( "Program HelloWorld" );
+
+            var word1 = _lex.Next();
+            var word2 = _lex.Next();
+
+            Assert.AreEqual( "Program", word1 );
+            Assert.AreEqual( "HelloWorld", word2 );
+        }
+
+        [TestMethod]
+        public void TestIdentificator()
+        {
+            _lex.LoadTextCode( "__az_AZ09" );
+
+            var word = _lex.Next();
+
+            Assert.AreEqual( "__az_AZ09", word );
+        }
+
+        [TestMethod]
+        public void TestSemicolon()
+        {
+            _lex.LoadTextCode( "18+2 - 3*4/ 5" );
+
+            Assert.AreEqual( "18", _lex.Next() );
+            Assert.AreEqual( "+", _lex.Next() );
+            Assert.AreEqual( "2", _lex.Next() );
+            Assert.AreEqual( "-", _lex.Next() );
+            Assert.AreEqual( "3", _lex.Next() );
+            Assert.AreEqual( "*", _lex.Next() );
+            Assert.AreEqual( "4", _lex.Next() );
+            Assert.AreEqual( "/", _lex.Next() );
+            Assert.AreEqual( "5", _lex.Next() );
+        }
+
+        [TestMethod]
+        public void TestEmpty()
+        {
+            _lex.LoadTextCode( "" );
+
+            var word = _lex.Next();
+
+            Assert.AreEqual( "", word );
+        }
+
+        [TestMethod]
+        public void TestEmptyWithWhitespace()
+        {
+            _lex.LoadTextCode( "  " );
+
+            var word = _lex.Next();
+
+            Assert.AreEqual( "", word );
+        }
+    }
+}
